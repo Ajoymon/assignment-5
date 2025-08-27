3c2cf0475049efffa2c1b1dea1e55bd3f328ed11
Ajoymon <apu14757@gmile.com>
2025-08-28 03:16:55 +0600

    js file section-2 complit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// section-1
 let count = 0;
 const loveCount = document.getElementById("love-count");
 const loveIcons = document.getElementsByClassName("hardPng");
@@ -7,3 +8,28 @@ for (const icon of loveIcons) {
     loveCount.textContent = count;
   });
 }
+
+// section-2
+let coins = 100;
+const coinDisplay = document.getElementById('coin-count');
+const callButtons = document.querySelectorAll('.call-btn');
+const callCost = 20;
+
+for (const btn of callButtons) {
+  btn.addEventListener('click', function() {
+    if (coins >= callCost) {
+      coins -= callCost;
+      coinDisplay.textContent = coins;
+
+      const card = btn.closest('.bg-white');
+      const number = card.querySelector('h1').innerText;
+      const description = card.querySelector('p').innerText;
+
+      alert(`${description} ${number}...`);
+    } else {
+      alert('You dont have enough coins.');
+    }
+  });
+}
+
+
