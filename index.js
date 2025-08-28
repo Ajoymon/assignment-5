6824d26a28ba659395e307e945c26dff25400fe7
Ajoymon <apu14757@gmile.com>
2025-08-29 01:52:38 +0600

    call fuction complit

--- index.js
+++ index.js
@@ -8,28 +8,53 @@ for (const icon of loveIcons) {
     loveCount.textContent = count;
   });
 }
-
 // section-2
 let coins = 100;
 const coinDisplay = document.getElementById('coin-count');
 const callButtons = document.querySelectorAll('.call-btn');
 const callCost = 20;
+const historyBox = document.querySelector(".mine-not");
+const clearBtn = document.querySelector(".clearBtn"); 
+
 
 for (const btn of callButtons) {
   btn.addEventListener('click', function() {
+    const card = btn.closest('.bg-white');
+    const title = card.querySelector("h2").innerText;
+    const number = card.querySelector("h1").innerText;
+    const description = card.querySelector("p").innerText;
+
     if (coins >= callCost) {
       coins -= callCost;
       coinDisplay.textContent = coins;
 
-      const card = btn.closest('.bg-white');
-      const number = card.querySelector('h1').innerText;
-      const description = card.querySelector('p').innerText;
-
+      
       alert(`${description} ${number}...`);
+
+      const now = new Date();
+      const time = now.toLocaleTimeString();
+
+      
+      const newLog = document.createElement("div");
+      newLog.className = "flex justify-between items-center p-4 bg-gray-50 rounded-2xl mb-2";
+      newLog.innerHTML = `
+        <div>
+          <h2 class="font-semibold text-[18px]">${title}</h2>
+          <p>${number}</p>
+        </div>
+        <p>${time}</p>
+      `;
+
+      historyBox.appendChild(newLog);
+
     } else {
-      alert('You dont have enough coins.');
+      alert('You don\'t have enough coins.');
     }
   });
 }
 
+
+clearBtn.addEventListener('click', function() {
+  historyBox.innerHTML = ''; 
+});
 
